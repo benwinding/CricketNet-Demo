4166441954c52d8d8f893ce70fa55bdc0e13b975
Ben <ben.winding@gmail.com>
2017-07-25 01:34:18 +0930

    Better zoom limits

--- src/Rendering/ControlsFactory.js
+++ src/Rendering/ControlsFactory.js
@@ -12,8 +12,8 @@ export default class ControlsFactory {
     controls.dampingFactor = 0.1;
     controls.rotateSpeed = 0.1;
     controls.minDistance = 10;
-    controls.maxDistance = 999;
-    controls.maxPolarAngle = THREE.Math.degToRad(90);
+    controls.maxDistance = 100;
+    controls.maxPolarAngle = THREE.Math.degToRad(88);
     return controls;
   }
 }
